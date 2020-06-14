87847f6e004d10d8448530019e9d69e9f874b877
AsadKarim <asadkarim33@gmail.com>
2020-06-14 13:05:01 -0700

    chapter 1 to 4 completed

---
+++ javascript/chapter 1 to 4 Assignments/apps.js
@@ -0,0 +1,262 @@
+//first chapter
+  
+
+
+// part 1
+// alert('Hello World');
+
+       //part 2
+
+// alert('Error! please enter a valid passsord');
+ 
+    //part3
+
+
+// alert('Welcome to js land...\nhappy coding' );
+     
+    // part4
+         
+// alert('Welcome to js land');
+
+        //part 5
+
+
+//alert('Hello i can runjs through my web browers console');
+// console.log('Hello i can runjs through my web browers console');
+
+                        
+                        
+                        // second Chapter
+
+        //part1
+
+// var Username ='';
+
+        //part2
+
+// var myName ="Asad karim Mughal"
+
+        //part3
+
+//message = "hello world";
+//alert(message);
+       
+       //part4
+       
+//   var name = "asad";
+//   var age ="22";
+//   var Education = "Saylani Mass training center";
+
+//   alert(name);
+//   alert(age);
+//   alert(Education);
+  
+       // part 5
+
+// alert('pizza \n pizz \n piz \n pi \n p'); 
+
+        // part 6
+
+//var email = "asadkarim33@gmail.com"
+
+//alert('my email address is ' + email);
+
+        //part7
+
+// var mybook = "A smarter way to learn javascript";
+// alert("i am trying to learn from the book " + mybook);
+
+        //part8
+     
+       // document.write('yah! i can write Html Content through javascript');
+
+     //  part9
+
+    //  var sign = "===================";
+    //  alert(sign);
+
+
+
+
+    //   chapter 3
+          //  part 1
+
+
+    // var age  = 22;
+    // alert("i am" + age +"year old");
+          
+          //  part 2 incomplete
+
+
+    // var age  = 22;
+    // alert("i am" + age +"year old");
+         //   part3
+
+
+     // var birthyear  = 1996;
+    // document.write('my birth year is '+ birthyear )
+    // document.write('Data type of my variable declaration is number');
+            
+            
+            
+//            part4
+
+
+//     var visitorName = "Asad";
+     //var product = "laptop";
+     
+   //  document.write(visitorName +"has order 2 " +product +" On Karim's Store ");
+
+
+
+
+
+
+   //   chapter 4
+
+
+
+  //  part
+
+ // var a = b = c = "asad";
+   
+     // part 2
+
+ // 5 illegal variable  5 legal variable 
+
+ //var  +asad           var asad             
+ //var  9               var &asad
+ //var  0asadkarim      var asad_karim          
+ //var  asad-karim      var asadKarim
+//var  alert            userAlert 
+
+
+//part 3
+
+//document.write(" A heading stating  Rules for naming JS variables <br> Variable names can only contain numbers, & and _ For example $my_1stVariable <br> Variables must begin with a $,or_ For example $name, _name or name <br> Variable names are case sensitive Variable names should not be JS keyword");
+
+
+//chapter 5
+        
+       // part 1
+
+//var asad = 3;
+//var ahmed = 5;
+
+//var aswad = asad + ahmed;
+
+//alert(aswad);
+
+//document.write("the sum of 3 and 5 " + aswad  );
+
+       // part 2
+
+//var asad = 3;
+//var ahmed = 5;
+
+//var aswad = asad - ahmed;
+
+//alert(aswad);
+
+//document.write("the - of 3 and 5 " - aswad  );
+
+
+//var asad = 3;
+//var ahmed = 5;
+
+//var aswad = asad * ahmed;
+
+//alert(aswad);
+
+//document.write("the Multiply of 3 and 5 " * aswad  );
+
+//var asad = 3;
+//var ahmed = 5;
+
+//var aswad = asad % ahmed;
+
+//alert(aswad);
+
+//document.write("the modulus of 3 and 5 " % aswad  );
+
+
+             // part 3
+
+ //var init_val = 5;
+ //var increment = 5 + 1;
+ //var Adding = increment + 7;
+ //var decrement = Adding - 1 ;
+ //var modulus = decrement % 2 ;
+//document.write("initial Value is " + 5 + " <br> value after increment is  "+increment+ "<br> value after addition is" +Adding+ " <br> value after decrement is" +decrement+" <br> the reminder is  "+modulus );         
+
+// part 4 
+
+//var ticket = 600;
+ 
+//var totalCost = 5 * ticket;
+//document.write("total cost to buy 5 tickets is "+ totalCost);
+
+//part 5
+
+//document.write("4 X 1 = 4 <br> 4 X 2 = 8 <br> 4 X 3 = 12 <br>4 X 4 = 16 <br> 4 X 5 = 20 <br> 4 X 6 = 24 <br> 4 X 7 = 28 <br> 4 X 8 = 32 <br> 4 X 9 = 36 <br> 4 X 10 = 40");
+
+                        //part 6
+
+// var cel_temp = prompt("Enter temperature in celcius");
+        // var far_temp = prompt("Enter temperature in farhenite");
+        // var c = (far_temp - 32) * 5 / 9;
+        // var f = (cel_temp / 5) + 32;
+        // document.write(cel_temp + "<sup>o</sup>C "+  "is " + f + "<sup>o</sup>F");
+        // document.write("<br>" + far_temp + "<sup>o</sup>F " + "is " + c + "<sup>o</sup>C");
+
+                        //part 7
+
+// var item_1Price = 600;
+        // var qty_Item1 = prompt("number of orders");
+        // var item_2Price = 500;
+        // var qty_Item2 = prompt("number of orders");
+        // var shipCharges = 200;
+        // var total_cost = (item_1Price * qty_Item1) + (item_2Price * qty_Item2) + shipCharges;
+        // document.write("<br>Price of item 1 is " + item_1Price);
+        // document.write("<br>Quantity of item 1 is " + qty_Item1);
+        // document.write("<br>Price of item 2 is " + item_2Price);
+        // document.write("<br>Quantity of item 2 is " + qty_Item2);
+
+        // document.write("<br>Total Cost of your order is " + total_cost);
+
+
+                //PART 8
+
+   //  var eng = prompt("Enter English marks");
+        //  var math = prompt("Enter math marks");
+        //  var urdu = prompt ("Enter urdu marks");
+        //  var eng1 = parseInt(eng);
+        //  var math1 = parseInt(math);
+        //  var urdu1 = parseInt(urdu);
+        //  var obt_marks = (eng1 + math1 + urdu1);
+        //  var total_marks = 300;
+        //  var per = (obt_marks / total_marks) * 100;
+        //  document.write("<br>Total Marks = " + total_marks);
+        //  document.write("<br>obtain marks = " + obt_marks);
+        //  document.write("<br>Percentage = " + per + "%");
+           
+                //part 9
+           
+            // var  ukPound = 160;
+            // var usDollor = 10;
+            // var total = (ukpound * 28) + (usDollor * 104.80);
+            // document.write("<h1>Currency in PKR</h1> ");
+            // document.write("<br>total Currency in PKR</h1>" + total);
+           
+           
+           // part10
+           
+           
+            // var currYear = prompt("Current year");
+            // var birthYear = prompt("write the birth year ");
+            // var age = currYear - birthYear;
+            // document.write("Current Year" + currYear);
+            // document.write("<br>Birth Year" + currYear);
+            // document.write("<br>Your age is " + age);
+
+            
