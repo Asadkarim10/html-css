2ea72d99a822a361ae17e2ee668c2fcc957733b7
AsadKarim <asadkarim33@gmail.com>
2020-06-15 13:30:21 -0700

    start working with react props

--- bootcamp/Bootcamp2020/class2nd/myproject/src/App.js
+++ bootcamp/Bootcamp2020/class2nd/myproject/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
   return (
     <div className="App">
     hello world
+     <Dinner   dishName="biryani" sour="tikka" sweet="zarda"/>
+     
     </div>
   );
 }
---
+++ bootcamp/Bootcamp2020/class2nd/myproject/src/dinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Dinner(props) {
+    return <div> <h2> I have done dinner {props.dishName} </h2>
+    <h2> I have done sweet {props.sweet} </h2>
+    <h2> I have done sour {props.sour} </h2> </div>
+}
+
+
+
+export default Dinner
